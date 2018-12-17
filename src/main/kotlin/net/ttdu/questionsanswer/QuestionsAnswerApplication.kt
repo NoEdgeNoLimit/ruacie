63acae5ec5375e1ba050bf0c1d90ba7615cd275d
jiyuhui <jiyuhui@jd.com>
2018-12-18 00:26:57 +0800

    初始化提交

--- src/main/kotlin/net/ttdu/questionsanswer/QuestionsAnswerApplication.kt
+++ src/main/kotlin/net/ttdu/questionsanswer/QuestionsAnswerApplication.kt
@@ -1,12 +1,12 @@
 package net.ttdu.questionsanswer
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class QuestionsAnswerApplication
-
-fun main(args: Array<String>) {
-	runApplication<QuestionsAnswerApplication>(*args)
+class QuestionsAnswerApplication  : SpringBootServletInitializer(){
 }
-
+fun main(args: Array<String>) {
+	SpringApplication.run(QuestionsAnswerApplication::class.java, *args)
+}
--- src/main/kotlin/net/ttdu/questionsanswer/controller/HelloWorldController.java
+++ src/main/kotlin/net/ttdu/questionsanswer/controller/HelloWorldController.java
@@ -1,12 +1,40 @@
 package net.ttdu.questionsanswer.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: jiyuhui
+ * @Date: 2018/12/13
+ * @Version 1.0
+ * @describe
+ */
+@Controller
 public class HelloWorldController {
+    @Value("${user.ok}")
+    private String name;
+
     @RequestMapping("/hello")
     public String index() {
-        return "吉玉辉";
+        return "Hello World";
+    }
+    @GetMapping("/")
+    public String homePage(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping("/static")
+    public String navigatorToStatic() {
+        return "redirect:/static.html";
     }
 }

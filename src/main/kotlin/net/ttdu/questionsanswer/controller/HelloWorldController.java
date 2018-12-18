4662b73324992be36a3db49852ee63520a533d8c
jiyuhui <jiyuhui@jd.com>
2018-12-19 01:30:25 +0800

    测试data

--- src/main/kotlin/net/ttdu/questionsanswer/controller/HelloWorldController.java
+++ src/main/kotlin/net/ttdu/questionsanswer/controller/HelloWorldController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.KeyFactory;
 /**
  * @Author: jiyuhui
  * @Date: 2018/12/13
@@ -30,11 +36,41 @@ public class HelloWorldController {
     @GetMapping("/")
     public String homePage(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException {
         model.addAttribute("name", name);
+        new ArrayList<>();
         return "index";
     }
 
     @RequestMapping("/static")
     public String navigatorToStatic() {
         return "redirect:/static.html";
+    }
+    @RequestMapping("/data")
+    public String datatest(){
+        // Instantiates a client
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+        // The kind for the new entity
+        String kind = "Task";
+        // The name/ID for the new entity
+        String name = "sampletask1";
+        // The Cloud Datastore key for the new entity
+        Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(name);
+
+        // Prepares the new entity
+        Entity task = Entity.newBuilder(taskKey)
+                .set("description", "Buy milk")
+                .build();
+
+        // Saves the entity
+        datastore.put(task);
+
+        System.out.printf("Saved %s: %s%n", task.getKey().getName(), task.getString("description"));
+
+        //Retrieve entity
+        Entity retrieved = datastore.get(taskKey);
+
+        System.out.printf("Retrieved %s: %s%n", taskKey.getName(), retrieved.getString("description"));
+
+        return null;
     }
 }

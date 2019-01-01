50518a4623df543cccecab83134114d8bbcb260d
jiyuhui <jiyuhui@jd.com>
2019-01-01 09:04:17 +0800

    测试data

---
+++ src/test/kotlin/net/ttdu/questionsanswer/QuickstartSample.java
@@ -0,0 +1,39 @@
+package net.ttdu.questionsanswer;
+
+import com.google.cloud.datastore.*;
+import org.junit.Test;
+
+public class QuickstartSample {
+    @Test
+    public void testData(){
+/*
+        // Instantiates a client
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
+        // The kind for the new entity
+        String kind = "Task";
+        // The name/ID for the new entity
+        String nameaa = "sampletask12";
+        // The Cloud Datastore key for the new entity
+        Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(nameaa);
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+        Key taskKey2 = datastore.allocateId(keyFactory.newKey());
+
+        // Prepares the new entity
+        Entity task = Entity.newBuilder(taskKey)
+                .set("description", "Buy milkaa")
+                .build();
+
+        // Saves the entity
+        datastore.put(task);
+
+        System.out.printf("Saved %s: %s%n", task.getKey().getName(), task.getString("description"));
+
+        //Retrieve entity
+        Entity retrieved = datastore.get(taskKey);
+
+        System.out.printf("Retrieved %s: %s%n", taskKey.getName(), retrieved.getString("description"));
+*/
+
+    }
+}
